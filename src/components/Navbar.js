b788694850b62a7d0dd191083f056e79fad74586
tuankhanh12 <73736182+tuankhanh12@users.noreply.github.com>
2024-07-17 01:34:44 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../contexts/AuthContext';
 
 const Navbar = () => {
-  const { isAuthenticated, logout } = useContext(AuthContext);
-
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      {isAuthenticated ? (
-        <>
-          <Link to="/cart">Cart</Link>
-          <button onClick={logout}>Logout</button>
-        </>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
-    </nav>
-  );
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
+
